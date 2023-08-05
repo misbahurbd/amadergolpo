299521906e775ea2714a479b7e32d5dbefbc45aa
Misbahur Rahman <misbahurbd@gmail.com>
2023-08-05 23:33:54 +0600

    update params on conversion page

--- hooks/useConversation.ts
+++ hooks/useConversation.ts
@@ -5,10 +5,9 @@ const useConversation = () => {
   const params = useParams()
 
   const conversationId = useMemo(() => {
-    if (!params.conversationId) {
+    if (!params?.conversationId) {
       return ''
     }
-
     return params.conversationId as string
   }, [params?.conversationId])
 
